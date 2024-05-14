d0914a728ddf7d2a14ad1b5d89560fde1e681cff
Iffu <shaikiffu@gmail.com>
2024-05-14 18:46:00 +0530

    Test

--- packages/login-module/lib/login-module.js
+++ packages/login-module/lib/login-module.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 function loginmodule() {
   // Define state variable 'count' and its setter function 'setCount'
   const [count, setCount] = useState(0);
-
+  console.log("Hello");
   return (
     <div>
       <p>Count: {count}</p>
@@ -14,7 +14,8 @@ function loginmodule() {
       {/* Button to decrement count */}
       <button onClick={() => setCount(count - 1)}>
         Decrement
-      </button>
+      </button> 
+    
     </div>
   );
 }
